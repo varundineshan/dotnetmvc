80783b5c80360230b473d6406e73ee4116682c62
varun dineshan <varun4dinesh@gmail.com>
2020-01-20 12:25:38 +0800

    made initial form template

--- HelloApp/Controllers/HomeController.cs
+++ HelloApp/Controllers/HomeController.cs
@@ -11,12 +11,7 @@ namespace HelloApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+       
 
         public IActionResult Index()
         {
@@ -28,10 +23,21 @@ namespace HelloApp.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpGet]
+        [RequireHttps]
+        public IActionResult UserForm()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
+        
+        [HttpPost]
+        public IActionResult Create(FormModel model)
+        {
+            ViewBag.name = model.name;
+            ViewBag.age = model.age;
+            ViewBag.email = model.email;
+            return View();
+        }
+
     }
 }
---
+++ HelloApp/Models/FormModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloApp.Models
+{
+    public class FormModel
+    {
+        public String name { get; set; }
+        public int age { get; set; }
+
+        public String email { get; set; }
+
+      
+    }
+}
